31cf39fd8aa07b5238bfe617045fdae4cf920c98
VinushiRanshila <139769880+VinushiRanshila@users.noreply.github.com>
2024-11-29 15:33:33 +0530

    First Commit

---
+++ src/test/java/OpenPage.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class OpenPage {
+    WebDriver driver;
+    @BeforeMethod
+    public void TestPage() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:3000");
+    }
+    @Test
+        public void TestTextBox(){
+            WebElement EnterName=driver.findElement(By.name("name"));
+            EnterName.sendKeys("Ann Perera");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
