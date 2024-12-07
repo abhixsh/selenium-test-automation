c25173193c6e6b2ce572c3426c2476cb22fd813f
Abishek Haththakage <aloka.abishek9@gmail.com>
2024-12-07 23:50:35 +0530

    Initialize Java project with Maven and add basic structure

---
+++ src/test/java/OpenPage.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class OpenPage {
+    WebDriver driver;
+    @BeforeMethod
+    public void TestPage() {
+        driver = new ChromeDriver();
+        driver.get("http://localhost:3000");
+    }
+    @Test
+    public void TestTextBox() throws InterruptedException {
+        WebElement EnterName=driver.findElement(By.name("name"));
+        EnterName.sendKeys("Ann Perera");
+        Thread.sleep(3000);
+        WebElement EnterEmail=driver.findElement(By.name("email"));
+        EnterEmail.sendKeys("Ann@gmail.com");
+        Thread.sleep(3000);
+        WebElement DatePicker=driver.findElement(By.name("date"));
+        DatePicker.sendKeys("02/02/2023");
+        Thread.sleep(3000);
+        WebElement EnterAddress=driver.findElement(By.name("address"));
+        EnterAddress.sendKeys("No:12/B,Thammita,Makevita");
+        Thread.sleep(3000);
+        WebElement SubmitButton=driver.findElement(By.tagName("button"));
+        SubmitButton.click();
+
+
+
+    }
+
+
+}
+
+
+
